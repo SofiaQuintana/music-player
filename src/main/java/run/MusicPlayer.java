9bde71968cf6e5683215458a0d7aa8c07f6405c9
SofiaQuintana <sofiaquintana2@gmail.com>
2021-08-30 21:00:55 -0600

    runnable

---
+++ src/main/java/run/MusicPlayer.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package run;
+
+import user_interface.Player;
+
+/**
+ *
+ * @author zofia-px
+ */
+public class MusicPlayer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Principal frame declaration
+        Player player = new Player();
+        //Frame visibility
+        player.setVisible(true);
+    }
+    
+}
